fc071e6b45c454637de3fdeb4cbb8f642966ff9c
TheWandere <martin.brandl@hotmail.de>
2023-10-21 09:07:27 +0200

    make scrollable grid responsive and first button draft, improve theming

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,9 +51,9 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.7.2")
+    implementation("androidx.activity:activity-compose:1.8.0")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
@@ -61,7 +61,8 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation("androidx.security:security-crypto:1.1.0-alpha02")
+    implementation("androidx.security:security-crypto:1.1.0-alpha06")
+    implementation("com.google.accompanist:accompanist-systemuicontroller:0.30.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/soundgem/MainActivity.kt
+++ app/src/main/java/com/example/soundgem/MainActivity.kt
@@ -7,14 +7,18 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FilledTonalButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -22,56 +26,69 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 data class AudioFile(var name: String, var content: String)
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        var file1 = AudioFile("Na hör mal", "content1")
-        var file2 = AudioFile("over 9000", "content1")
-        var file3 = AudioFile("NOOOO", "content1")
-        var file4 = AudioFile("rickRoll", "content1")
+        val file1 = AudioFile("Na hör mal", "content1")
+        val file2 = AudioFile("over 9000", "content1")
+        val file3 = AudioFile("NOOOO", "content1")
+        val file4 = AudioFile("rickRoll", "content1")
         val files = mutableListOf<AudioFile>(file1, file2, file3, file4)
+        (1..20).map { files.add(AudioFile("test", "content")) }
         setContent {
-            Column (
-                verticalArrangement= Arrangement.SpaceBetween,
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(colorResource(R.color.white))
-            ) {
-                Header()
-                AudioGrid(files)
-                Footer()
+            Scaffold(
+                topBar = { Header() },
+                bottomBar = { Footer() }
+            ) { innerPadding ->
+                AudioGrid(files = files, padding = innerPadding)
             }
         }
     }
 
 
 @Composable
-fun AudioGrid(files: MutableList<AudioFile>) {
+fun AudioGrid(files: MutableList<AudioFile>, padding: PaddingValues) {
     LazyVerticalGrid(
         columns = GridCells.Fixed( count = 2 ),
+        modifier = Modifier
+            .padding(padding)
+            .fillMaxHeight()
     ) {
         files.map { file -> item {
             Box(
-                modifier = Modifier.height(120.dp),
+                modifier = Modifier
+                    .height(160.dp)
+                    .padding(vertical = 10.dp),
                 contentAlignment = Alignment.Center
             ) {
-                FilledTonalButton(
-                    colors = ButtonDefaults.filledTonalButtonColors(
-                        containerColor = colorResource(R.color.primary_300),
-                        contentColor = Color.Black
-                    ),
-                    modifier = Modifier
-                        .height(80.dp)
-                        .width(150.dp),
-                    onClick = { println(file.content) }) {
+                Column(
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    FilledTonalButton(
+                        colors = ButtonDefaults.filledTonalButtonColors(
+                            containerColor = colorResource(R.color.primary_300),
+                            contentColor = Color.Black
+                        ),
+                        elevation= ButtonDefaults.buttonElevation(12.dp),
+                        modifier = Modifier
+                            .height(80.dp)
+                            .width(80.dp),
+                        onClick = { println(file.content) }){
+                        Text(
+                            text="\uD83D\uDE00",
+                            fontSize = 32.sp,
+                            textAlign = TextAlign.Center
+                        )
+                    }
                     Text(
                         text = file.name,
-                        color = colorResource(R.color.primary_900),
-                        fontSize = 18.sp,
                     )
                 }
             }
@@ -82,10 +99,13 @@ fun AudioGrid(files: MutableList<AudioFile>) {
 
 @Composable
 fun Header() {
+    rememberSystemUiController().setSystemBarsColor(
+        color = colorResource(R.color.primary_500)
+    )
     Box(
         modifier = Modifier
             .fillMaxWidth()
-            .height(45.dp)
+            .height(60.dp)
             .background(colorResource(R.color.primary_500)),
         contentAlignment = Alignment.Center,
     ) {
@@ -103,7 +123,7 @@ fun Footer() {
     Box(
         modifier = Modifier
             .fillMaxWidth()
-            .height(65.dp)
+            .height(70.dp)
             .background(colorResource(R.color.primary_500)),
         contentAlignment = Alignment.Center,
     ) {
