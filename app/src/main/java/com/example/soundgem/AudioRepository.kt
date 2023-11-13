2de14d7aa4dce36dfa5f0da745001cd855ec22bf
ALsJourney <alsjourneydev@hotmail.com>
2023-11-13 14:27:32 +0100

    Deleted unnecessary AudioRepository Interface

--- app/src/main/java/com/example/soundgem/AudioRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.example.soundgem
-
-import io.github.jan.supabase.storage.BucketItem
-
-interface AudioRepository {
-    suspend fun fetchFilesFromBucket(): List<BucketItem>
-    suspend fun getAllFilesFromBucket(): List<BucketItem>
-}
