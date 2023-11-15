92bae203f7ea6f58ba51f572cdb045702f65ccc1
ALsJourney <alsjourneydev@hotmail.com>
2023-11-15 21:22:19 +0100

    changed minSdk to 33 so that we can install it on our pleb phones

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.soundgem"
-        minSdk = 34
+        minSdk = 33
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
